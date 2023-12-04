3cb60487d8bf461d2450b101abd3bc8417589eef
Dallas Carlson <118072502+DalPalCarl@users.noreply.github.com>
2023-12-04 06:32:51 -0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -4,6 +4,10 @@ import requests
 from bs4 import BeautifulSoup as bsoup
 import csv
 import re
+import time
+
+http = 'http://'
+https = 'https://'
 
 nodes = []
 init_url = input("Enter Initial URL Here: ")
@@ -16,10 +20,7 @@ def crawl_page(soup, c_url):
         url = link_element['href']
 
         if re.search(r"https://www.", url) and url not in visited:
-            node = {"node1" : current_url, "node2" : url}
-            nodes.append(node)
-            urls.append(url)
-            print(url)
+            return
 
 
 def get_html(url):
@@ -30,17 +31,32 @@ def get_html(url):
         return ''
 
 while len(urls) != 0:
+    time.sleep(1)
     current_url = urls.pop()
     visited.append(current_url)
 
+    if len(urls) > 50:
+        break
+
+    if get_html(current_url) == '':
+        continue
     response = requests.get(current_url)
     soup = bsoup(response.content, "html.parser")
 
     crawl_page(soup, current_url)
-            
+
+    link_elements = soup.select("a[href]")
+    for link_element in link_elements:
+        url = link_element['href']
+        if url not in visited:
+            if http in url or https in url:
+                node = {"node1" : current_url, "node2" : url}
+                nodes.append(node)
+                urls.append(url)
+                print(url)
+    print(len(urls))
 
 with open('nodes.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
     for node in nodes:
         writer.writerow(node.values())
-
